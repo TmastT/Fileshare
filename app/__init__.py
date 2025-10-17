2e084c3cba6c2f2e24e23faedb183a74682ee41e
tmasT <tmast3000@gmail.com>
2025-10-17 22:19:59 +0200

    Base template and User class#

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from .models import db
 
 def create_app():
     app = Flask(__name__)
 
-    # Config
     app.config.from_pyfile('config.py', silent=True)
 
-    # Blueprints
+    @app.route('/index')
+    def index():
+        return render_template('base.html', app_name=app.config.get('APP_NAME', 'File Sharing App'))
+
 
     return app
